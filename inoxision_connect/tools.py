ff070536e7e37c85d113e39501e002480175b891
David Malinowski <dm@itsdave.de>
2021-09-28 12:39:25 +0200

    Konvertieren zu Tif und erzeugen von Attachement Uploads

--- inoxision_connect/tools.py
+++ inoxision_connect/tools.py
@@ -6,6 +6,13 @@ import io
 import requests
 import ftplib
 import urllib.parse
+from wand.image import Image
+ 
+#ny = Image(filename ='pdf new york.pdf')
+#ny_converted = ny.convert('jpg')
+#for img in ny_converted.sequence:
+#  page = ny(image = img)
+#  page.save(filename='new york pdf to image.jpg')
 
 
 @frappe.whitelist()
@@ -16,59 +23,58 @@ def archive_to_inoxision(doc, method=None):
     active_doctypes = frappe.get_all("Inoxision Connect Settings Active Doctype", fields="doctype_link", as_list=True)
     if (doc.doctype,) not in active_doctypes:
         return None
-
     do_archive(doc.doctype, doc.name)
-    print(doc.doctype)
-    print(doc.name)
+
 
 @frappe.whitelist()
 def do_archive(doctype, name):
     settings = frappe.get_single("Inoxision Connect Settings")
-    if settings.archive_enabled != 1:
-        return False
-    if settings.inputpath == "" or settings.inputpattern == "" or settings.outputarchivename == "":
+    if settings.inputpath == "" or settings.outputarchivename == "":
         frappe.msgprint("Keine Archivierung möglich. Inoxision Connect Settings unvollständig.")
         return False
-        
-    http_base = settings.http_endpoint
+
     language = frappe.get_single("System Settings")
 
     html = frappe.get_print(doctype, name, None, None, no_letterhead="no_letterhead")
     filename = "{name}.pdf".format(name=name.replace(" ", "-").replace("/", "-"))
-    #f = io.BytesIO(get_pdf(html))
-    f = io.BytesIO(b"")
+    f_pdf = io.BytesIO(get_pdf(html))
     session = ftplib.FTP(settings.server, settings.user, settings.password)
+
+
+    if settings.convert_to_tiff == 1:
+        converted_return = convert_to_tif(f_pdf, filename, settings)
+        f = converted_return[0]
+        filename = converted_return[1]
 
     if settings.path:
         if settings.path >= "/":
             session.cwd(settings.path)
     session.storbinary("STOR " + filename , f)
+    control_file = get_inoxision_control_file_content(doctype, name, filename, settings)
+    session.storbinary("STOR " + filename + ".txt", control_file)
 
     f = None
+    control_file = None
 
     attachments = get_attachments(doctype, name)
     for a in attachments:
-        # In [2]: do_archive("Sales Invoice", "SINV-250014")
-        #[{'name': 'b9dee55174', 'file_name': 'datasheet.pdf', 'file_url': '/private/files/datasheet.pdf', 'is_private': 1}]
-
-        #In [3]: frappe.utils.get_bench_path()
-        #Out[3]: '/Users/dave/frappe-bench13'
-
-        #In [4]: frappe.utils.get_site_path()
-        #Out[4]: './dev12.erpnext.itsdave.de'
         f = frappe.utils.get_bench_path() + "/sites" + frappe.utils.get_site_path()[1:] + a.file_url
         file = open(f, "rb") 
         session.storbinary("STOR " + a.file_name, file)     # send the file
+        print(a.filename)
+        control_file = get_inoxision_control_file_content(doctype, name, a.filename, settings)
+        session.storbinary("STOR " + a.file_name + ".txt", file)
+
         file.close()
-    kopf_zeile = "[Execute]\nInputType=IMAGE\nInputPath=" + str(settings.inputpath)+"\nInputPattern=" + str(settings.inputpattern) + "\nAutoExecute=" + str(settings.autoexecute)+"\nOutputArchiveName=" + str(settings.outputarchivename) +"\n"
-    schluss_zeile = "\nSwapProcess=1\nCreateFullTextChecked=1\nDeleteConfigFile=1"                                    
-    mitte = get_keyword(doctype,name)
-    textfile = kopf_zeile + mitte + schluss_zeile
-    print(textfile)
+    
     session.quit()
     
-def get_keyword(doctype, name):
-    
+
+def get_inoxision_control_file_content(doctype, name, filename, settings):
+    control_file_content = ""
+    kopf_zeile = "[Execute]\nInputType=IMAGE\nInputPath=" + str(settings.inputpath)+"\nInputPattern=" + str(filename) + "\nAutoExecute=" + str(settings.autoexecute)+"\nOutputArchiveName=" + str(settings.outputarchivename) +"\n"
+    schluss_zeile = "\nSwapProcess=1\nCreateFullTextChecked=1\nDeleteConfigFile=1"                                    
+
     keyword_list = frappe.get_all("Inoxision Connect Settings Field Assignment",filters = {"doctype_link":doctype},fields= ["destination_field","source_field"])
     doctype_doc = frappe.get_doc(doctype,name)
     keyword_zeile = "KeywordValues="
@@ -78,12 +84,20 @@ def get_keyword(doctype, name):
             keyword_zeile += "Belege." + keyword["destination_field"] + "|" + a +"|"
         else:
             continue
-    return keyword_zeile
+
+    control_file_content = kopf_zeile + keyword_zeile + schluss_zeile
+    bytes_io = io.BytesIO(control_file_content.encode())
+    return bytes_io
     
-    
-    
-        
-    #print(keyword_list)
-    # SwapProcess=1
-    # CreateFullTextChecked=1
-    # DeleteConfigFile=1
+def convert_to_tif(input_file, filename, settings):
+    if settings.resolution > 0:
+        resolution = settings.resolution
+    else:
+        resolution = 200
+    output_file = io.BytesIO()
+    filename = filename.replace(".pdf",".tif")
+    with Image(file=input_file, resolution=resolution) as original:
+        with original.convert("tif") as converted:
+            converted.save(file=output_file)
+    output_file.seek(0)
+    return (output_file, filename)
